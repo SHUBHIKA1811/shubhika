b9a289faf8c1e69368f38cf3d7d36acdddbbfa4b
SHUBHIKA1811 <shubhikagoyal22@gmail.com>
2019-08-20 06:08:16 -0700

    this is my first commit

---
+++ weathercon/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WeatherconConfig(AppConfig):
+    name = 'weathercon'
---
+++ weathercon/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+
+
+  
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+
+
+
+
